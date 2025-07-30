48af2e0692ef5492e9b29608d30357a9bd5f6aca
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-07-30 19:31:32 +0200

    working on dice roller

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <time.h>
+#include <string.h>
+#include <windows.h>
+int roll_dice(int max_num);
+
+int main(){
+    char command[] = "first roll : ";
+    system("cls");
+    while (1==1)
+    {
+        int max_num;
+        printf(command);
+        scanf("%d", &max_num);
+        system("cls");
+        strcpy(command, "next roll: ");
+        Sleep(1000);
+        printf("result : %d\n", roll_dice(max_num));
+    }
+    
+ return 0;
+}
 
 int roll_dice(int max_num){
     int result;
@@ -11,17 +31,3 @@ int roll_dice(int max_num){
      }
     return result;
 }
-
-int main(){
-    while (1==1)
-    {
-        int max_num;
-        printf("max number : ... \n");
-        scanf("%d", &max_num);
-        system("cls");
-        printf("result : %d\n", roll_dice(max_num));
-    }
-    
- return 0;
-}
-
