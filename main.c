e4cea4f556ece7ef3ca0841f6a8a432461cd9bf4
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-07-30 16:34:26 +0200

    added loop

--- main.c
+++ main.c
@@ -6,14 +6,21 @@ int roll_dice(int max_num){
     int result;
     srand(time(NULL)); 
      result = rand() % max_num;
+     if(result<1){
+        result=1;
+     }
     return result;
 }
 
 int main(){
-    int max_num;
-    printf("max number : ... \n");
-    scanf("%d", &max_num);
-    printf("result : %d", roll_dice(max_num));
+    while (1==1)
+    {
+        int max_num;
+        printf("max number : ... \n");
+        scanf("%d", &max_num);
+        printf("result : %d", roll_dice(max_num));
+    }
+    
  return 0;
 }
 
