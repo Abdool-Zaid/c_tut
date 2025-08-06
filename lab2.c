3d288ddbf55a8e0dd5e8c641f907b9885274314e
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-06 21:15:16 +0200

    update

---
+++ lab2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    int i = 2 ;
+    i = "34";
+
+    printf("i is now %d\n");
+    
+
+}
--- sanf_data1.c
+++ sanf_data1.c
@@ -14,6 +14,7 @@ int main(){
     printf("size of c : %lld and its pointer : %lld\n ", sizeof(c), sizeof(&c));
     printf("size of f : %lld and its pointer : %lld\n ", sizeof(f), sizeof(&f));
     printf("size of df : %lld and its pointer : %lld\n ", sizeof(df), sizeof(&df));
+    // printf("size of void : %lld\n ", sizeof(void));
     printf("size of 100 : %lld\nsize of 0xffffffff : %lld\nsize of 0xffffffffff : %lld\n ", sizeof(100), sizeof(0xffffffff),sizeof(0xffffffffff));
 
 }
