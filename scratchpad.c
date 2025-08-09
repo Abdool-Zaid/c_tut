7eff5504fa9a0294ac9de683bff129392a7f0bbd
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-09 17:21:07 +0200

    armstrong numbers

--- scratchpad.c
+++ scratchpad.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
- 
 int main()
 {
-    float principal, rate, time;
- 
-    // Get user input
-    printf("Enter the principal amount: ");
-    scanf("%f",&principal);
-    printf("Enter the rate of interest: ");
-    scanf("%f",&rate);
-    printf("Enter the time: ");
-    scanf("%f",&time);
- 
-    float  simple_interest, amount;
- 
-    // Calculate simple interest and amount
-    simple_interest = ((principal*rate*time)/100);
-    amount = simple_interest + principal;
- 
-    // Print the output
-    printf("Simple Interest = %f \nAmount = %f",simple_interest, amount);
-    return 0;
+int a = 15, b = 0;
+printf("Addition: %d\n", a + b);
+printf("Subtraction: %d\n", a -b);
+printf("Multiplication: %d\n", a* b);
+printf("Division: %d\n", a / b);
+printf("Modulus: %d\n", a % b);
+return 0;
 }
