c059f047296df8597eeacc3e4d3a46558ac28218
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-09 10:42:23 +0200

    simple interest program

--- scratchpad.c
+++ scratchpad.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-void counter() {
-    static int count = 0; // Retains value between calls
-    count++;
-    printf("Count: %d\n", count);
-}
-int main() {
-    counter(); // Prints: Count: 1
-    counter(); // Prints: Count: 2
-    counter(); // Prints: Count: 3
+ 
+int main()
+{
+    float principal, rate, time;
+ 
+    // Get user input
+    printf("Enter the principal amount: ");
+    scanf("%f",&principal);
+    printf("Enter the rate of interest: ");
+    scanf("%f",&rate);
+    printf("Enter the time: ");
+    scanf("%f",&time);
+ 
+    float  simple_interest, amount;
+ 
+    // Calculate simple interest and amount
+    simple_interest = ((principal*rate*time)/100);
+    amount = simple_interest + principal;
+ 
+    // Print the output
+    printf("Simple Interest = %f \nAmount = %f",simple_interest, amount);
     return 0;
 }
