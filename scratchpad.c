15676fd5405a3a5a99c4c86175f4d1241b1562fd
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-19 18:25:00 +0200

    bit field in union

--- scratchpad.c
+++ scratchpad.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+union bit_ex{
+    unsigned char a:1;
+    unsigned char b:1;
+};
+
+
 int main()
 {
-char *a[1] = {"hello"};
-printf("%s", a[0]);
-return 0;
+  union bit_ex u1;
+  u1.b =0;
+  u1.a = 1;
+    printf("u1.a : %d\nu1.b : %d\n", u1.a,u1.b);
+
+    return 0;
 }
