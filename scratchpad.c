bce68022158bc392c313b983d301b29edfd6156e
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-23 20:10:03 +0200

    sub string search

--- scratchpad.c
+++ scratchpad.c
@@ -1,16 +1,40 @@
-#include <stdio.h>
+/*  
+ * C program to accept a string and a substring and
+ * check if the substring is present in the given string
+ */
+#include<stdio.h>
  
-int main() {
-    int num = -4;  // Example negative number
-    int result = num >> 1;
+int main()
+{
+    char str[80], search[10];
+    int count1 = 0, count2 = 0, i, j, flag;
  
-    printf("Original number: %d\n", num);
-    printf("After right shift: %d\n", result);
- 
-    if (result < 0)
-        printf("Right shift is Arithmetic (sign bit preserved)\n");
+    printf("Enter a string:");
+    gets(str);
+    printf("Enter search substring:");
+    gets(search);
+    while (str[count1] != '\0')
+        count1++;
+    while (search[count2] != '\0')
+        count2++;
+    for (i = 0; i <= count1 - count2; i++)
+    {
+        for (j = i; j < i + count2; j++)
+        {
+            flag = 1;
+            if (str[j] != search[j - i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            break;
+    }
+    if (flag == 1)
+        printf("SEARCH SUCCESSFUL!");
     else
-        printf("Right shift is Logical (zeros filled in)\n");
+        printf("SEARCH UNSUCCESSFUL!");
  
     return 0;
 }
