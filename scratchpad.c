19988aade8af446c769f721518dd64a7367cbe22
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-23 10:50:00 +0200

    update

--- scratchpad.c
+++ scratchpad.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
  
 int main() {
     int num = -4;  // Example negative number
@@ -15,20 +14,3 @@ int main() {
  
     return 0;
 }
-=======
-union bit_ex{
-    unsigned char a:1;
-    unsigned char b:1;
-};
-
-
-int main()
-{
-  union bit_ex u1;
-  u1.b =0;
-  u1.a = 1;
-    printf("u1.a : %d\nu1.b : %d\n", u1.a,u1.b);
-    printf("size of u1 : %lld\n", sizeof(u1));
-    return 0;
-}
->>>>>>> ad0ac73421d14a96bf51eac037427b0ee6381ddc
