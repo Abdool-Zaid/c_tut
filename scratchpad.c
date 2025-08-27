a37b84d430a4988301552174d1486337254517ad
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-27 16:22:45 +0200

    array notation is reversible

--- scratchpad.c
+++ scratchpad.c
@@ -1,74 +1,11 @@
-/*
- * C program to find whether a given year is a leap year or not
- *
- * status codes 
- * 0 == success
- * 1 == failure
- * 
- * authur Abdool Zaid
- *
- */
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
 
+int main (){
 
-typedef unsigned char sts;
-sts get_year();
-sts is_leap_year();
-void print_results(sts result);
-int year; // outside main to be used as global 
-int main()
-{
-    
-    if (get_year() == 0){
-        print_results(is_leap_year());
-        
-    }else{
-        printf("please enter a valid year");
+    char arr[10]= {1,2,3,4,5,6,7,8,9,0};
+    for (size_t i = 0; i < sizeof(arr); i++)
+    {
+        printf("%lld : %d\n", i, i[arr]);
     }
-    // isdigit(year) will use this later for checking that the user inputted a number
-
-
     
-}
-    
-    
-sts get_year(){
-    // sts status =1;// TODO: use this when you have type checking 
-    sts status = 0;
-    printf("Enter a year \n");
-    scanf("%d", &year);
-
-    return status;
-}
-
-sts is_leap_year(){
-    sts status = 2;
-if ((year % 400) == 0)
-            status = 0;
-         else if ((year % 100) == 0)
-            status = 1;
-         else if ((year % 4) == 0)
-            status = 0;
-         else
-            status = 1;
-         
-         return status;
-        }
-void print_results(sts result){
-
-switch (result){
-    case 0:
-        printf("%d is a leap year \n", year);
-    break;
-            
-    case 1:
-        printf("%d is a not leap year \n", year);
-    break;
-              
-    default:
-        printf("something went wrong\n");
-    break;
-    }
-            
 }
